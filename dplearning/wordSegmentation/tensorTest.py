f3481df87099e12712d110f07cfed428c9122e93
hklhai <hkhai@outlook.com>
2018-08-14 08:20:55 +0800

    gan demo

--- dplearning/wordSegmentation/tensorTest.py
+++ dplearning/wordSegmentation/tensorTest.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import tensorflow as tf
 
 """
 import os  
@@ -7,19 +6,11 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"]='1' # 这是默认的显示等级，显示所
 os.environ["TF_CPP_MIN_LOG_LEVEL"]='2' # 只显示 warning 和 Error   
 os.environ["TF_CPP_MIN_LOG_LEVEL"]='3' # 只显示 Error   
 """
-
+import tensorflow as tf
 v = tf.Variable([1, 2])
 c = tf.constant([3, 3])
-# 减法op
 sub = tf.subtract(v, c)
-# 加法op
-add = tf.add(sub, v)
-
-# 变量需要初始化操作
 init = tf.global_variables_initializer()
-
-# Session()后的括号不能省
 with tf.Session() as sess:
     sess.run(init)
     print(sess.run(sub))
-    print(sess.run(add))
